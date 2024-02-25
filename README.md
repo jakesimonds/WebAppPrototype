30eca491da5c70d26e48bfa42699a05e192ad709
Jake <jakesimonds@gmail.com>
2024-02-25 18:35:40 -0500

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# hw-blog
+# 5610 Project Prototype
+
+Ignore structure and such, just used this blog since I had it handy. 
+
+
+
+
 
 ## How to run locally
 
@@ -10,30 +16,7 @@ export FLASK_APP=flaskr
 flask run --debug
 ```
 
-Debug flag is optional if you don't wanna change anything and see those changes in real time. 
 
+3. log in, and go to 'Drive the Robot' 
 
-
-## New Functionality: rank by word count
-
-I added an endpoint (just added it to __init__.py file since this was an experiment) that performs a SQL query to get the longest blog posts by word count, and then made a template word-count.html that takes that json information and displays with with flask's render_template(). 
-
-It's not very pretty, but the basic idea could then be fiddled with the sort ascending/descending as well as by other features (newest/oldest, etc)
-
-Entries:
-![](wordcount2.png)
-
-Ranked by word count: 
-![](wordcount.png)
-
-If you were to then edit a post or add a post, the rankings would be adjusted. 
-
-## New Design
-
-I made buttons for the top header bar. 
-
-![](css.png)
-
-## Acknowledgements
-
-I used chatGPT for syntax help, both giving it code I wrote that wasn't working to find errors and describing in natural language a couple things I wanted (such as SQL query to rank by word count), and then iterated with it to get what I was after. 
+4. Set up 'server' and boot up Wonder Dash robot. 
--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, render_template, jsonify
 from . import db
+import requests
 
 
 def create_app(test_config=None):
@@ -59,6 +60,64 @@ def create_app(test_config=None):
         return render_template('word-count.html', posts=posts_with_word_counts)
 
 
+    @app.route('/robot')
+    def robot():
+        # Get the database connection
+        database_connection = db.get_db()
 
+        # Perform the SQL query to get word counts
+        posts_with_word_counts = database_connection.execute(
+            'SELECT id, title, LENGTH(body) - LENGTH(REPLACE(body, " ", "")) + 1 AS word_count '
+            'FROM post '
+            'ORDER BY word_count DESC'
+        ).fetchall()
+        posts_with_word_counts = [dict(row) for row in posts_with_word_counts]
+
+        data = jsonify(posts_with_word_counts)
+        
+
+        return render_template('robot.html')
+
+    @app.route('/connect')
+    def connect():
+        print("connect hit, sending")
+        res = requests.get('http://localhost:5555/connect')
+        print("sent")
+        return jsonify(res.json())
+
+    @app.route('/forward')
+    def forward():
+        print("before request to 5555")
+        response = requests.get('http://localhost:5555/forward')
+        print("after request to 5555")
+        return jsonify(response.json())
+
+    @app.route('/right')
+    def right():
+        print("before request to 5555")
+        response = requests.get('http://localhost:5555/right')
+        print("after request to 5555")
+        return jsonify(response.json())
+    
+    @app.route('/left')
+    def left():
+        print("before request to 5555")
+        response = requests.get('http://localhost:5555/left')
+        print("after request to 5555")
+        return jsonify(response.json())
+
+    @app.route('/back')
+    def back():
+        print("before request to 5555")
+        response = requests.get('http://localhost:5555/back')
+        print("after request to 5555")
+        return jsonify(response.json())
+    
+    @app.route('/disconnect')
+    def disconnect():
+        print("before request to 5555")
+        response = requests.get('http://localhost:5555/disconnect')
+        print("after request to 5555")
+        return jsonify(response.json())
 
     return app
